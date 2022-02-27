a68142256097a624c833a6926ce855c3c74f0dcb
macbook <a.grek@headsandhands.ru>
2022-02-28 00:43:55 +0300

    second commit

--- src/test/java/tests/APITest.java
+++ src/test/java/tests/APITest.java
@@ -1,14 +1,9 @@
 package tests;
 
 import io.qameta.allure.Description;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.http.ContentType;
-import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.given;
 import static tests.Methods.findUserByName;
 
 public class APITest{
